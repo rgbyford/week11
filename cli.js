6b8e46fce97ac3c91637c6f015ce1b5759c8ab4a
Roger <rgbyford@gmail.com>
2018-10-25 19:49:51 -0500

    first version

---
+++ cli.js
@@ -0,0 +1,23 @@
+require ('tv.js');
+
+const sShowActor = process.argv[2];
+args = process.argv.length - 3; // node, prog name, show/actor
+
+let sItemName = "";
+for (let i = 0; i < args; i++) {
+    sItemName += process.argv[3 + i];
+    if (i < args -1) {
+        sItemName += ' ';
+    }
+}
+if (sShowActor === "show") {
+//    findShow (sItemName);
+    console.log (`Show: ${sItemName}`)
+}
+else if (sShowActor === 'actor') {
+//    findPerson (sItemName);
+    console.log (`Actor: ${sItemName}`)
+}
+else {
+    console.log ("Need to specify show or actor");
+}
